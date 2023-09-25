417e47b6cc2fe41f897743969208a9cbb25f2031
AntonMakesGames <antonmakesgames@gmail.com>
2023-09-25 20:18:59 +0200

    real inital commit

---
+++ ts/vec3.ts
@@ -0,0 +1,56 @@
+class vec3 {
+	x : number;
+	y: number;
+	z: number;
+
+	constructor(x : number, y : number,z :number) {
+		if ( x === null)
+		{
+			this.x = 0;
+			this.y = 0;
+			this.z = 0;
+		} else {
+			this.x = x;
+			this.y = y;
+			this.z = z;
+		}
+	}
+
+	length() {
+		return Math.sqrt(this.x * this.x + this.y * this.y + this.z * this.z);
+	}
+
+	normalize() {
+		const length = this.length();
+		this.x = this.x / length;
+		this.y = this.y / length;
+		this.z = this.z / length;
+
+		return this;
+	}
+
+	norm() {
+		const length = this.length();
+		return new vec3(this.x / length, this.y /length, this.z / length);
+	}
+
+	dot(other : vec3) {
+		return this.x * other.x + this.y * other.y + this.z * other.z;
+	}
+
+	cross(other : vec3) {
+		return new vec3(
+			this.y * other.z - this.z * other.y,
+			this.z * other.x - this.x * other.z,
+			this.x * other.y - this.y * other.x,
+		);
+	}
+
+	add(other : vec3) {
+		return new vec3(this.x + other.x, this.y * other.y, this.z * other.z);
+	}
+
+	scale(scale : number) {
+		return new vec3(this.x * scale, this.y * scale, this.z * scale);
+	}
+}
