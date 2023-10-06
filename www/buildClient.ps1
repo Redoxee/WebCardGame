53399ae9638b667c1fd971a854f67f678250cbc1
AntonMakesGames <antonmakesgames@gmail.com>
2023-10-06 16:20:32 +0200

    Rebuilding the project

--- www/buildClient.ps1
+++ www/buildClient.ps1
@@ -1,44 +1,27 @@
 $ErrorActionPreference = "Stop"
-# From https://rkeithhill.wordpress.com/2009/08/03/effective-powershell-item-16-dealing-with-errors/
-function CheckLastExitCode {
-    param ([int[]]$SuccessCodes = @(0), [scriptblock]$CleanupScript=$null)
 
-    if ($SuccessCodes -notcontains $LastExitCode) {
-        if ($CleanupScript) {
-            "Executing cleanup script: $CleanupScript"
-            &$CleanupScript
-        }
-        $msg = @"
-EXE RETURNED EXIT CODE $LastExitCode
-CALLSTACK:$(Get-PSCallStack | Out-String)
-"@
-        throw $msg
-    }
-}
-
-$workingFolder = "./dist"
-$destinationFolder = "./build"
+$scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
+echo $scriptPath
+$workingFolder = ".\dist"
+$destinationFolder = ".\build"
 
 # Clean folders
 echo "cleaning $workingFolder"
 Get-ChildItem $workingFolder | Remove-Item -Force -Verbose
+
 echo "cleaning $destinationFolder"
 Get-ChildItem $destinationFolder | Remove-Item -Force -Verbose
-CheckLastExitCode
 
 # Compile typescript
 echo "Compiling code"
-tsc --build --verbose
-CheckLastExitCode
+tsc --build --verbose 
 
 # Build client
 echo "Building project"
-npx parcel build ./index.html
-CheckLastExitCode
+npx parcel build --public-url /WebCardGame/www/build ./index.html
 
 # Copying to build folder
 echo "Coyping from $workingFolder to $destinationFolder"
 cp "$workingFolder/*" $destinationFolder
-CheckLastExitCode
 
 echo "Build complete"
