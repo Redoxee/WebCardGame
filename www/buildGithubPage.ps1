ff82f2795598b2208a3fd0a39eec6c03ca5f157f
AntonMakesGames <antonmakesgames@gmail.com>
2023-10-06 16:20:32 +0200

    Rebuilding the project

---
+++ www/buildGithubPage.ps1
@@ -0,0 +1,29 @@
+$ErrorActionPreference = "Stop"
+
+$scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
+echo $scriptPath
+$workingFolder = ".\dist"
+$destinationFolder = ".\build"
+
+$projectUrl = "/WebCardGame/www/build"
+
+# Clean folders
+echo "cleaning $workingFolder"
+Get-ChildItem $workingFolder | Remove-Item -Force -Verbose
+
+echo "cleaning $destinationFolder"
+Get-ChildItem $destinationFolder | Remove-Item -Force -Verbose
+
+# Compile typescript
+echo "Compiling code"
+tsc --build --verbose 
+
+# Build client
+echo "Building project"
+npx parcel build --public-url $projectUrl ./index.html
+
+# Copying to build folder
+echo "Coyping from $workingFolder to $destinationFolder"
+cp "$workingFolder/*" $destinationFolder
+
+echo "Build complete"
