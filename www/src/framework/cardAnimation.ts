905091ddf5b47ef9860eeb1261d45a37dc81cc3d
AntonMakesGames <antonmakesgames@gmail.com>
2023-10-26 23:11:25 +0200

    Added the option to translate cards without rotating them

--- www/src/framework/cardAnimation.ts
+++ www/src/framework/cardAnimation.ts
@@ -68,9 +68,9 @@ class CardLerpAnimation extends CardAnimation {
 	bezierParams : IBezierParams;
 	direction : Vec2;
 
-	constructor(target : ICardPresentation, rotationFactor : number) {
+	constructor(target : ICardPresentation) {
 		super(target);
-		this.rotationFactor = rotationFactor;
+		this.rotationFactor = 0;
 		this.duration = 0;
 		this.p0 = Vec2.Zero.clone();
 		this.p1 = Vec2.Zero.clone();
@@ -82,7 +82,7 @@ class CardLerpAnimation extends CardAnimation {
 		this.id = uniqueId();
 	};
 	
-	StartAnimation(p0 : Vec2, p1 : Vec2, speed : number, bezierParams : IBezierParams) {
+	StartAnimation(p0 : Vec2, p1 : Vec2, speed : number, bezierParams : IBezierParams, rotationFactor : number) {
 		this.p0 = p0.clone();
 		this.p1 = p1.clone();
 		const distance = (Vec2.sub(p1, p0).length());
@@ -92,6 +92,7 @@ class CardLerpAnimation extends CardAnimation {
 		this.travel = Vec2.sub(p1, p0);
 		this.direction = this.travel.norm();
 		this.bezierParams = bezierParams;
+		this.rotationFactor = rotationFactor;
 		
 		if (!cardAnimations.find((e)=>e.id === this.id)) {
 			cardAnimations.push(this);
@@ -107,13 +108,14 @@ class CardLerpAnimation extends CardAnimation {
 			this.target.root.style.left = `${this.p1.x}px`;
 			this.target.root.style.top = `${this.p1.y}px`;
 			this.target.currentPosition = this.p1;
-			this.target.LookToward(Vec2.Zero);
+			if (this.rotationFactor > 0) {
+				this.target.LookToward(Vec2.Zero);
+			}
 			this.target.dispatchEvent(this.endEvent);
 			return true;
 		}
 	
 		const t = this.elapsedTime / this.duration;
-		const rotationFactor = this.rotationFactor;
 		
 		const transformedTime = cubicInterpolationBezier(t, this.bezierParams);
 	
@@ -122,8 +124,11 @@ class CardLerpAnimation extends CardAnimation {
 		this.target.root.style.top = `${currentPos.y}px`;
 		this.target.currentPosition = currentPos;
 		// this.target.dispatchEvent(new CustomEvent('animationFrame'));
-		const transformedAcceleration = cubicInterpolationBezierFirstDerivative(t, this.bezierParams).scale(rotationFactor);
-		this.target.LookToward(this.direction.scale(transformedAcceleration.y));
+		if(this.rotationFactor > 0) {
+			const transformedAcceleration = cubicInterpolationBezierFirstDerivative(t, this.bezierParams).scale(this.rotationFactor);
+			this.target.LookToward(this.direction.scale(transformedAcceleration.y));
+		}
+
 		return false;
 	}
 }
